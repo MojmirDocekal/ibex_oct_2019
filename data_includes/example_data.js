924c92dabc4f247cc29b41d0a5f2d7ef505a7929
Mojmir Docekal <docekal@phil.muni.cz>
2019-10-28 15:24:09 +0100

    ch1

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -28,19 +28,19 @@ var items = [ ["sep", "Separator", { }],
           ["intro", "Form", {continueMessage: "Pro vstup do experimentu, klikněte zde", html: { include: "example_intro.html" }}],
 
 
-["practice", aj, {s: {html: "<p>Kontext: Aleš ... </p> <p> Aleš: 'Plavecký závod nakonec vyhrál dokonce Mirek.'</p><p><i>Věta je v daném kontextu nepřijatelná: výraz '</i>dokonce<i>' se neslučuje s vysokou kontextovou pravděpodobností Mirkova vítězství; vyberte jako odpověď jednu z možností v levém konci škály (nejspíš 1).</i></p>"}, leftComment: "(v daném kontextu je věta naprosto nepřijatelná)", rightComment: "(v daném kontextu je věta zcela přijatelná)"}],
+["practice", aj, {s: {html: "<p>Kontext: Aleš si právě přečetl v novinách, že 'Ve městě Er nikdo nepřečetl víc než 20 knih.' a komentuje to následující větou. </p> <p> Aleš: 'Takže někteří obyvatelé města Er přečetli i 25 knih.'</p><p><i>Alešův komentář ukazuje, že původní informaci špatně pochopil; vyberte jako odpověď jednu z možností v levém konci škály (nejspíš 1).</i></p>"}, leftComment: "(Aleš pochopil vše naprosto špatně)", rightComment: "(Aleš pochopil vše naprosto správně)"}],
 
 
           ["practiceover", "Message", {continueMessage: "Klikněte zde pro pokračování experimentu", html: ["div", ["p", "Zde končí přípravná část experimentu. Klikněte níže pro vstup do experimentu."]]}],
 
-[["first-item1-at-most", 1], aj, {s: {html: "<p>Kontext: Aleš čte nápis na balení potravin: 'Toto jídlo může obsahovat nanejvýš 100 gramů cukru' a komentuje to následující větou.</p><p>Aleš: 'Takže v tom jídle se občas může objevit i 110 gramů cukru.'</p>"}, leftComment: "(Aleš pochopil vše naprosto správně)", rightComment: "(Aleš pochopil vše naprosto špatně)"}],
+[["first-item1-at-most", 1], aj, {s: {html: "<p>Kontext: Aleš čte nápis na balení potravin: 'Toto jídlo může obsahovat nanejvýš 100 gramů cukru' a komentuje to následující větou.</p><p>Aleš: 'Takže v tom jídle se občas může objevit i 110 gramů cukru.'</p>"}, leftComment: "(Aleš pochopil vše naprosto špatně)", rightComment: "(Aleš pochopil vše naprosto správně)"}],
 
-  [["first-item1-no-more", 1], aj, {s: {html: "<p>Kontext: Aleš čte nápis na balení potravin: 'Toto jídlo může obsahovat ne víc než 100 gramů cukru' a komentuje to následující větou.</p><p>Aleš: 'Takže v tom jídle se občas může objevit i 110 gramů cukru.'</p>"}, leftComment: "(Aleš pochopil vše naprosto správně)", rightComment: "(Aleš pochopil vše naprosto špatně)"}],
+  [["first-item1-no-more", 1], aj, {s: {html: "<p>Kontext: Aleš čte nápis na balení potravin: 'Toto jídlo může obsahovat ne víc než 100 gramů cukru' a komentuje to následující větou.</p><p>Aleš: 'Takže v tom jídle se občas může objevit i 110 gramů cukru.'</p>"}, leftComment: "(Aleš pochopil vše naprosto špatně)", rightComment: "(Aleš pochopil vše naprosto správně)"}],
 
-  [["first-item1-fewer", 1], aj, {s: {html: "<p>Kontext: Aleš čte nápis na balení potravin: 'Toto jídlo může obsahovat méně než 100 gramů cukru' a komentuje to následující větou.</p><p>Aleš: 'Takže v tom jídle se občas může objevit i 110 gramů cukru.'</p>"}, leftComment: "(Aleš pochopil vše naprosto správně)", rightComment: "(Aleš pochopil vše naprosto špatně)"}],
+  [["first-item1-fewer", 1], aj, {s: {html: "<p>Kontext: Aleš čte nápis na balení potravin: 'Toto jídlo může obsahovat méně než 100 gramů cukru' a komentuje to následující větou.</p><p>Aleš: 'Takže v tom jídle se občas může objevit i 110 gramů cukru.'</p>"}, leftComment: "(Aleš pochopil vše naprosto špatně)", rightComment: "(Aleš pochopil vše naprosto správně)"}],
 
 
-["filler1-good", aj, {s: {html: "<p>Kontext: Aleš si právě přečetl v novinách, že 'Tři současní čeští básníci napsali (každý) přesně jednu knihu básní.' a komentuje to následující větou.</p><p>Aleš: 'Takže ti tři básníci napsali přesně tři knihy básní.'</p>"}, leftComment: "(Aleš pochopil vše naprosto správně)", rightComment: "(Aleš pochopil vše naprosto špatně)"}],
-["filler2-bad", aj, {s: {html: "<p>Kontext: čte informaci o městě Er: 'Radnice města Er je vysoká 40 metrů a každá budova v městě Er je nižší než radnice' a komentuje to následující větou.</p><p>Aleš: 'Takže některé budovy v městě Er, včetně radnice, mají 45 metrů a víc'</p>"}, leftComment: "(Aleš pochopil vše naprosto správně)", rightComment: "(Aleš pochopil vše naprosto špatně)"}],
+["filler1-good", aj, {s: {html: "<p>Kontext: Aleš si právě přečetl v novinách, že 'Tři současní čeští básníci napsali (každý) přesně jednu knihu básní.' a komentuje to následující větou.</p><p>Aleš: 'Takže ti tři básníci napsali přesně tři knihy básní.'</p>"}, leftComment: "(Aleš pochopil vše naprosto špatně)", rightComment: "(Aleš pochopil vše naprosto správně)"}],
+["filler2-bad", aj, {s: {html: "<p>Kontext: čte informaci o městě Er: 'Radnice města Er je vysoká 40 metrů a každá budova v městě Er je nižší než radnice' a komentuje to následující větou.</p><p>Aleš: 'Takže některé budovy v městě Er, včetně radnice, mají 45 metrů a víc'</p>"}, leftComment: "(Aleš pochopil vše naprosto špatně)", rightComment: "(Aleš pochopil vše naprosto správně)"}],
 
 ];
